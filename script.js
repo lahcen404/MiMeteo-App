e3ea587bdabe0a8ca283b68654161e24bb67f023
lahcen404 <lahcen.maskour2003@gmail.com>
2025-01-23 17:54:07 +0100

    display of first section in app for every city by search

---
+++ script.js
@@ -0,0 +1,22 @@
+const apiKey = 'cbefa706b94ec8836e327138b61fdef2';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+const searchCity = document.getElementById('search-input');
+const searchButton = document.querySelector('.btn-primary');
+
+async function getWeather(city) {
+    const response = await fetch(apiUrl + city + '&appid=' + apiKey);
+    const data = await response.json();
+    // update elemnts with weather data
+    document.querySelector('#city-name').innerHTML = data.name;
+    document.querySelector('#temp').innerHTML = Math.round(data.main.temp) + " °C";
+    document.querySelector(".desc").innerHTML = data.weather[0].description;
+    document.querySelector('#humidity').innerHTML = data.main.humidity + " %";
+    document.querySelector('#wind').innerHTML = data.wind.speed + " km/h";
+    const weatherIcon= document.querySelector(".icon-weather-main")=data.weather[0].icon;
+    weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    console.log(data);
+}
+
+searchButton.addEventListener('click', () => {
+    getWeather(searchCity.value.trim());
+});

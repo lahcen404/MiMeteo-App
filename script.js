f957300791a76e5b537bd787ac3c6843ba44f9ee
lahcen404 <lahcen.maskour2003@gmail.com>
2025-01-24 10:09:44 +0100

    Fix first page of displaying Weather data of city  with fixing Display of Icon

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const searchCity = document.getElementById('search-input');
 const searchButton = document.querySelector('.btn-primary');
 
 async function getWeather(city) {
-    const response = await fetch(apiUrl + city + '&appid=' + apiKey);
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     const data = await response.json();
     // update elemnts with weather data
     document.querySelector('#city-name').innerHTML = data.name;
     document.querySelector('#temp').innerHTML = Math.round(data.main.temp) + " °C";
-    document.querySelector(".desc").innerHTML = data.weather[0].description;
+    document.querySelector("#desc").innerHTML = data.weather[0].description;
     document.querySelector('#humidity').innerHTML = data.main.humidity + " %";
     document.querySelector('#wind').innerHTML = data.wind.speed + " km/h";
-    const weatherIcon= document.querySelector(".icon-weather-main")=data.weather[0].icon;
-    weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    document.querySelector(".icon-weather-main").src= `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    
     console.log(data);
 }
 
 searchButton.addEventListener('click', () => {
     getWeather(searchCity.value.trim());
-});
+})

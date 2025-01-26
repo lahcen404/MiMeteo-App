61e1f86b5106aaa525b153d86645477bbd91bc9d
lahcen404 <lahcen.maskour2003@gmail.com>
2025-01-26 16:42:13 +0100

    change 5 days weather to a loop

--- script.js
+++ script.js
@@ -3,19 +3,27 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 const apiFive = 'https://api.openweathermap.org/data/2.5/forecast?units=metric&q='
 const searchCity = document.getElementById('search-input');
 const searchButton = document.querySelector('.btn-primary');
+const geoButton = document.querySelector('.btn-geo');
 
 async function getWeather(city) {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    const data = await response.json();
-    // update elemnts with weather data
-    document.querySelector('#city-name').innerHTML = data.name;
-    document.querySelector('#temp').innerHTML = Math.round(data.main.temp) + " °C";
-    document.querySelector("#desc").innerHTML = data.weather[0].description;
-    document.querySelector('#humidity').innerHTML = data.main.humidity + " %";
-    document.querySelector('#wind').innerHTML = data.wind.speed + " km/h";
-    document.querySelector(".icon-weather-main").src= `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    
-    console.log(data);
+    try {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        if (!response.ok) {
+            throw new Error('City not found');
+        }
+        const data = await response.json();
+        // update elements with weather data
+        document.querySelector('#city-name').innerHTML = data.name;
+        document.querySelector('#temp').innerHTML = Math.round(data.main.temp) + " °C";
+        document.querySelector("#desc").innerHTML = data.weather[0].description;
+        document.querySelector('#humidity').innerHTML = data.main.humidity + " %";
+        document.querySelector('#wind').innerHTML = data.wind.speed + " km/h";
+        document.querySelector(".icon-weather-main").src= `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        console.log(data);
+    } catch (error) {
+        alert('City not found..  enter a valid city name!!');
+        console.error(error);
+    }
 }
 
 async function getFiveDaysWeather(city){
@@ -23,25 +31,13 @@ async function getFiveDaysWeather(city){
     const fiveDaysData = await forecastResponse.json();
     console.log(fiveDaysData);
 
-const days = fiveDaysData.list.filter((item) => item.dt_txt.includes('12:00:00'));
-document.querySelector('#day1-name').innerHTML=new Date(days[0].dt_txt).toLocaleDateString('en-US', {weekday: 'long'});
-document.querySelector('#day2-name').innerHTML=new Date(days[1].dt_txt).toLocaleDateString('en-US', {weekday: 'long'});
-document.querySelector('#day3-name').innerHTML=new Date(days[2].dt_txt).toLocaleDateString('en-US', {weekday: 'long'});
-document.querySelector('#day4-name').innerHTML=new Date(days[3].dt_txt).toLocaleDateString('en-US', {weekday: 'long'});
-document.querySelector('#day5-name').innerHTML=new Date(days[4].dt_txt).toLocaleDateString('en-US', {weekday: 'long'});
+    const days = fiveDaysData.list.filter((item) => item.dt_txt.includes('12:00:00'));
 
-document.querySelector('.temp1').innerHTML=Math.round(days[0].main.temp) + " °C";
-document.querySelector('.temp2').innerHTML=Math.round(days[1].main.temp) + " °C";
-document.querySelector('.temp3').innerHTML=Math.round(days[2].main.temp) + " °C";
-document.querySelector('.temp4').innerHTML=Math.round(days[3].main.temp) + " °C";
-document.querySelector('.temp5').innerHTML=Math.round(days[4].main.temp) + " °C";
-
-document.querySelector('.icon1').src= `http://openweathermap.org/img/wn/${days[0].weather[0].icon}.png`;
-document.querySelector('.icon2').src= `http://openweathermap.org/img/wn/${days[1].weather[0].icon}.png`;
-document.querySelector('.icon3').src= `http://openweathermap.org/img/wn/${days[2].weather[0].icon}.png`;
-document.querySelector('.icon4').src= `http://openweathermap.org/img/wn/${days[3].weather[0].icon}.png`;
-document.querySelector('.icon5').src= `http://openweathermap.org/img/wn/${days[4].weather[0].icon}.png`;
-}
+    for (let i = 0; i < 5; i++) {
+        document.querySelector(`#day${i + 1}-name`).innerHTML = new Date(days[i].dt_txt).toLocaleDateString('en-US', {weekday: 'long'});
+        document.querySelector(`.temp${i + 1}`).innerHTML = Math.round(days[i].main.temp) + " °C";
+        document.querySelector(`.icon${i + 1}`).src = `http://openweathermap.org/img/wn/${days[i].weather[0].icon}.png`;
+    }}
 
 
 
@@ -49,4 +45,6 @@ searchButton.addEventListener('click', () => {
     getWeather(searchCity.value.trim());
     getFiveDaysWeather(searchCity.value.trim())
 })
+
+
 
